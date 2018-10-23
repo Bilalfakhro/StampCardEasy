e071e1edca2892506c45283bdd14d83aed3da737
Bilalfakhro <bilal.fakhro@hotmail.se>
2018-10-23 21:02:18 +0200

    Initial Commit

---
+++ StampCardEasy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StampCardEasy
+//
+//  Created by Bilal Fakhro on 2018-10-23.
+//  Copyright © 2018 Bilal Fakhro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
